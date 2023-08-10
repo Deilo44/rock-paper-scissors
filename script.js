44fd5229dbed9927ed58b4bfd34bafbf0cd68cbe
danokirror@gmail.com <danokirror@gmail.com>
2023-08-10 15:08:57 +0300

    fixed a few errors in my play round function

--- script.js
+++ script.js
@@ -8,22 +8,22 @@ function playRound(playerSelection, computerSelection){
     if(playerSelection.toLowerCase()===computerSelection.toLowerCase()){
         return "!!!!Draw!!!!";
     }
-    else if(playerSelection.toLowerCase()===paper && computerSelection.toLowerCase()===rock){
+    else if(playerSelection.toLowerCase()==="paper" && computerSelection.toLowerCase()==="rock"){
         return "You Win! Paper beats Rock";
     }
-    else if(playerSelection.toLowerCase()===rock && computerSelection.toLowerCase()===paper){
+    else if(playerSelection.toLowerCase()==="rock" && computerSelection.toLowerCase()==="paper"){
         return "You Lose! Paper beats Rock";
     }
-    else if(playerSelection.toLowerCase()===rock && computerSelection.toLowerCase()===scissors){
+    else if(playerSelection.toLowerCase()==="rock" && computerSelection.toLowerCase()==="scissors"){
         return "You Win! Rock beats Scissors";
     }
-    else if(playerSelection.toLowerCase()===scissors && computerSelection.toLowerCase()===rock){
+    else if(playerSelection.toLowerCase()==="scissors" && computerSelection.toLowerCase()==="rock"){
         return "You Lose! Rock beats Scissors";
     }
-    else if(playerSelection.toLowerCase()===paper && computerSelection.toLowerCase()===scissors){
+    else if(playerSelection.toLowerCase()==="paper" && computerSelection.toLowerCase()==="scissors"){
         return "You Lose! Scissors beats paper";
     }
-    else if(playerSelection.toLowerCase()===scissors && computerSelection.toLowerCase()===paper){
+    else if(playerSelection.toLowerCase()==="scissors" && computerSelection.toLowerCase()==="paper"){
         return "You Win! Scissors beats paper";
     }
 }
