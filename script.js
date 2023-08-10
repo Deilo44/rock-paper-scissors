1db8852fbf361b4e0d98a404965f3511f92d05a5
danokirror@gmail.com <danokirror@gmail.com>
2023-08-10 14:25:59 +0300

    writen the playround function

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ function getComputerChoice(){
     let randomindex= Math.floor(Math.random()*choices.length);
     return choices[randomindex];
 }
+
+function playRound(playerSelection, computerSelection){
+    computerSelection = getComputerChoice();
+    if(playerSelection.toLowerCase()===computerSelection.toLowerCase()){
+        return "!!!!Draw!!!!";
+    }
+    else if(playerSelection.toLowerCase()===paper && computerSelection.toLowerCase()===rock){
+        return "You Win! Paper beats Rock";
+    }
+    else if(playerSelection.toLowerCase()===rock && computerSelection.toLowerCase()===paper){
+        return "You Lose! Paper beats Rock";
+    }
+    else if(playerSelection.toLowerCase()===rock && computerSelection.toLowerCase()===scissors){
+        return "You Win! Rock beats Scissors";
+    }
+    else if(playerSelection.toLowerCase()===scissors && computerSelection.toLowerCase()===rock){
+        return "You Lose! Rock beats Scissors";
+    }
+    else if(playerSelection.toLowerCase()===paper && computerSelection.toLowerCase()===scissors){
+        return "You Lose! Scissors beats paper";
+    }
+    else if(playerSelection.toLowerCase()===scissors && computerSelection.toLowerCase()===paper){
+        return "You Win! Scissors beats paper";
+    }
+}
